48d28a858720f7f3a10fd34e19208abb2968eef8
V.Kukh <emdasoftware@gmail.com>
2022-10-12 17:49:24 +0300

    add_usecases+repository_and_test_impl_of_it

--- app/src/main/java/com/emdasoft/pokemonapp/presentation/PokemonListViewModel.kt
+++ app/src/main/java/com/emdasoft/pokemonapp/presentation/PokemonListViewModel.kt
@@ -1,9 +1,6 @@
 package com.emdasoft.pokemonapp.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.emdasoft.pokemonapp.api.model.PokeApiResponse
-import com.emdasoft.pokemonapp.api.model.PokeResult
 import com.emdasoft.pokemonapp.data.RepositoryImpl
 import com.emdasoft.pokemonapp.domain.GetPokemonListUseCase
 
@@ -14,10 +11,6 @@ class PokemonListViewModel : ViewModel() {
 
     private val getPokemonListUseCase = GetPokemonListUseCase(repository)
 
-//    val pokemonList = MutableLiveData<List<PokeResult>>()
     val pokemonList = getPokemonListUseCase.getPokemonList(200, 0)
 
-//    fun getPokemonList(){
-//        getPokemonListUseCase.getPokemonList(1154,0)
-//    }
 }
